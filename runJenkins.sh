f44267842f7a668d93a0ea73370808a5126d88b1
Tjark Smalla <tjark.smalla@gmx.de>
2017-03-25 21:39:01 +0100

    Add color to runJenkins output to make successfull start more clear

--- runJenkins.sh
+++ runJenkins.sh
@@ -1,3 +1,3 @@
 mkdir -p persistence
 docker rm my_jenkins
-docker run -d --name my_jenkins -p 8080:8080 -p 50000:50000 -v $(pwd)/persistence/:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock custom-jenkins 
+docker run -d --name my_jenkins -p 8080:8080 -p 50000:50000 -v $(pwd)/persistence/:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock custom-jenkins && echo -e "\e[32mJenkins started\e[39m"
